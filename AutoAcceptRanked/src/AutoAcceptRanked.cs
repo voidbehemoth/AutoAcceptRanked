0ff0eeba4faeeaf3b8eabae12a855ba68b83b723
VoidBehemoth <VoidBehemoth@gmail.com>
2023-10-17 14:18:46 -0600

    Add patch

--- AutoAcceptRanked/src/AutoAcceptRanked.cs
+++ AutoAcceptRanked/src/AutoAcceptRanked.cs
@@ -1,4 +1,8 @@
-﻿using SML;
+﻿using Home.Common;
+using SML;
+using HarmonyLib;
+using Home.Messages.Outgoing;
+using Services;
 
 namespace AutoAcceptRanked
 {
@@ -7,16 +11,28 @@ namespace AutoAcceptRanked
     {
         public static void Start()
         {
-            AutoAcceptRanked.Utils.Logger.Log(AutoAcceptRanked.ModInfo.PLUGIN_NAME + "has loaded!");
+            AutoAcceptRanked.Utils.Logger.Log(ModInfo.PLUGIN_NAME + "has loaded!");
         }
     }
 
+    [HarmonyPatch(typeof(RankedTimerPanelController), nameof(RankedTimerPanelController.HandleAcceptRankedPopup)]
+    public class RankedTimerPatch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix() 
+        {
+            RankedQueueResponseMessage msg = new RankedQueueResponseMessage(RankedQueueResponseMessage.ResponseType.AcceptQueue);
+            Service.Home.NetworkService.SendMessage(msg);
+
+            return false; // Skip original method
+        }
+    }
 
     public static class ModInfo
     {
         public const string PLUGIN_GUID = "AutoAcceptRanked";
 
-        public const string PLUGIN_NAME = "AutoAcceptRanked";
+        public const string PLUGIN_NAME = "Auto Accept Ranked";
 
         public const string PLUGIN_VERSION = "1.0.0";
     }
