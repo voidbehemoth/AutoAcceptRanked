c63b47a6e09a54bb8eb9e26cc2fd5eb2d2fe99ad
VoidBehemoth <VoidBehemoth@gmail.com>
2023-10-17 13:58:33 -0600

    Add project files.

---
+++ AutoAcceptRanked/src/AutoAcceptRanked.cs
@@ -0,0 +1,23 @@
+﻿using SML;
+
+namespace AutoAcceptRanked
+{
+    [Mod.SalemMod]
+    public class Main
+    {
+        public static void Start()
+        {
+            AutoAcceptRanked.Utils.Logger.Log(AutoAcceptRanked.ModInfoPLUGIN_NAME + "has loaded!");
+        }
+    }
+
+
+    public static class ModInfo
+    {
+        public const string PLUGIN_GUID = "AutoAcceptRanked";
+
+        public const string PLUGIN_NAME = "AutoAcceptRanked";
+
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
